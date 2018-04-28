f21da6a7557673d13de7ecffcdec52cce8a0e406
Jailsonmc <jailsonmc@gmail.com>
2018-04-28 18:40:28 +0100

    Falta implementar pythonds

--- Passageiro.py
+++ Passageiro.py
@@ -2,6 +2,8 @@ import datetime
 import pytz
 from time import gmtime, strftime
 
+from pythonds.basic import queue
+
 class Passageiro:
 
     name = ""
@@ -19,7 +21,19 @@ class Passageiro:
     def ciclo_in(self):
         return self.__ciclo_in
 
+class Balcao:
 
+    def __init__(self, n_balcao, fila, inic_atend, passt_atend, numt_bag, tempt_esp, bag_utemp):
+        self.__n_balcao = n_balcao
+        self.__fila = fila
+        self.__inic_atend = inic_atend
+        self.__passt_atend = passt_atend
+        self.__numt_bag = numt_bag
+        self.__tempt_esp = tempt_esp
+        self.__bag_utemp = bag_utemp
+
+    def muda_inic_atend(self, inic_atend_pass):
+        self.__inic_atend = inic_atend_pass
 
 if __name__ == '__main__' :
 
