51918f3c9ac57e704449a3af918605b3e4766403
Jailsonmc <jailsonmc@gmail.com>
2018-05-01 20:23:23 +0100

    Fix

--- Herc.py
+++ Herc.py
@@ -1,4 +1,3 @@
-
 class Contacto:
 
     def __init__(self, nome, email, telefone, aniversario):
@@ -9,7 +8,7 @@ class Contacto:
 
 class Aluno(Contacto):
 
-    def __init__(self, n_aluno, curso, ano, turma, notas, nome,  email, telefone, aniversario):
+    def __init__(self, n_aluno, curso, ano, turma, notas, nome, email, telefone, aniversario):
         super(). __init__(nome, email, telefone, aniversario)
         self.n_aluno = n_aluno
         self.curso = curso
@@ -17,9 +16,16 @@ class Aluno(Contacto):
         self.turma = turma
         self.notas = notas
 
-
 class Professor(Contacto):
 
     def __init__(self, n_professor, nome,  email, telefone, aniversario):
         super(). __init__(nome, email, telefone, aniversario)
         self.nome = n_professor
+
+if __name__ == '__main__':
+
+    notas = [ 3, 5, 12, 19]
+
+    aluno = Aluno("12312", "Automação Industrial", "2012", "D", notas, "Jailson", "jailsonmc@gmail.com", "123 432 432", "1964-12-12")
+
+    print(aluno.aniversario)
